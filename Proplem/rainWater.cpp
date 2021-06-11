3d6a3313c1061d2b08a06e6e3f590af5921c25e0
farukhpatel <farukhpatel0804@gmail.com>
2021-06-11 14:48:58 +0530

    new update

---
+++ Proplem/rainWater.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[]={3,1,2,4,0,1,3,2};
+    int n=sizeof(a)/sizeof(a[0]);
+    int l[n];  //left array
+    int r[n];  //right array
+    l[0]=a[0];
+    r[n-1]=a[n-1];
+    for(int i=1;i<=n-1;i++){
+        l[i]=max(l[i-1],a[i]);  
+    }
+    for(int i=n-1;i>=1;i--){
+        r[i]=max(r[i+1],a[i]);   
+    }
+    for(int i=0;i<=n-1;i++){
+        cout<<l[i];
+        cout<<r[i];
+    }
+    int ans=0;
+    for(int i=0;i<n;i++){
+        ans+=min(l[i],r[i])-a[i];
+    }
+    cout<<"Ans:-"<<ans;
+
+    
+
+}
---
+++ string/str.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+//     string str="aba";
+//     string str2;
+//     // cout<<str.length();=6
+//     //print a reverse string
+//     for(int i=str.length()-1;i>=0;i--){
+//         cout<<str[i]<<endl;
+//         str2=str2+str[i];
+//     }
+//    if(str==str2){
+//       cout<<"Palindrome"<<endl;
+//    }else{
+//    cout<<"Not";}  
+string random="farukh123_123";
+string newRandom;
+// for(int i=0;i<=random.length()-1;i++){
+//    if((i>=0 && i<=9) || (i>='a' && i<='z') || (i>='A' && i<='Z') ){
+//        cout<<random[i];
+//    }
+//    break;
+// }
+for(char i='a' i<='z',i++){
+    cout<<i;
+}
+
+   return 0;
+   
+}
