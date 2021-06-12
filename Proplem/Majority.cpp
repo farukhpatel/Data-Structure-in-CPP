54d1e35e07694ff428cf6e2d98da9430acca8d08
farukhpatel <farukhpatel0804@gmail.com>
2021-06-12 20:32:31 +0530

    patter start

--- Proplem/Majority.cpp
+++ Proplem/Majority.cpp
@@ -1,19 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a[]={3,2,1,4,1};
+    int a[]={3,3,3,3,3,3,33,3,3,2,1,4,1,2,2,2};
     int n=sizeof(a)/sizeof(a[0]);
     int count=0;
-    int max;
+    int max=0;
+    int oldcount=0;
     for(int i=0;i<n;i++){
        for(int j=0;j<n;j++){
             if(a[i]==a[j]){
                count++; 
             }
-            if(count>n/2){
-                max=a[i];
-            }
         }
+        if(count>n/2  && count>oldcount){
+                max=a[i];
+                oldcount=count;
+                count=0;
+            }
+         
     }
     if(max){
         cout<<"majority number:-"<<max;
